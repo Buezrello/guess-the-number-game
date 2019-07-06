0e6c469c4d5f4349889daa2325f949412a2c9841
Igor Gindin <igindin@gmail.com>
2019-07-06 19:29:33 +0300

    game logic finished

--- console/src/main/java/igor/learnprogramming/console/ConsoleNumberGuess.java
+++ console/src/main/java/igor/learnprogramming/console/ConsoleNumberGuess.java
@@ -1,10 +1,15 @@
 package igor.learnprogramming.console;
 
+import igor.learnprogramming.IGame;
+import igor.learnprogramming.IMessageGenerator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+
+import java.util.Scanner;
 
 @Component
 public class ConsoleNumberGuess {
@@ -12,9 +17,40 @@ public class ConsoleNumberGuess {
     // == constants ==
     private static final Logger log = LoggerFactory.getLogger(ConsoleNumberGuess.class);
 
+    // == fields ==
+    @Autowired
+    private IGame game;
+    @Autowired
+    private IMessageGenerator messageGenerator;
+
+    // == events ==
     @EventListener(ContextRefreshedEvent.class)
     public void start() {
         log.info("start() ==> Container ready to use.");
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println(messageGenerator.getMainMessage());
+            System.out.println(messageGenerator.getResultMessage());
+
+            int guess = scanner.nextInt();
+            scanner.nextLine();
+            game.setGuess(guess);
+            game.check();
+
+            if (game.isGameWon() || game.isGameLost()) {
+                System.out.println(messageGenerator.getResultMessage());
+                System.out.println("Play again y/n?");
+
+                String playAgainString = scanner.nextLine().trim();
+                if (!playAgainString.equals("y")) {
+                    break;
+                }
+
+                game.reset();
+            }
+        }
     }
 
 }
