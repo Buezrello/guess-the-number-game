b6b92a70393c265604f4a42f13277ec1011744cb
Igor Gindin <igindin@gmail.com>
2019-07-06 20:49:03 +0300

    using Properties

--- core/src/main/java/igor/learnprogramming/config/GameConfig.java
+++ core/src/main/java/igor/learnprogramming/config/GameConfig.java
@@ -2,15 +2,21 @@ package igor.learnprogramming.config;
 
 import igor.learnprogramming.GuessCount;
 import igor.learnprogramming.MaxNumber;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@PropertySource("classpath:config/game.properties")
 public class GameConfig {
 
     // == fields ==
-    private int maxNumber = 25;
-    private int guessCount = 8;
+    @Value("${game.maxNumber:20}")
+    private int maxNumber;
+
+    @Value("${game.guessCount:5}")
+    private int guessCount;
 
     // == bean methods ==
     @Bean
