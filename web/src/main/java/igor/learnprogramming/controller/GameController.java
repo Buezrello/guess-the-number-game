09fd25987bdb36af1d91e9001abd90dcd4fe4243
Igor Gindin <igindin@gmail.com>
2019-07-28 23:32:34 +0300

    Play Thymeleaf template

---
+++ web/src/main/java/igor/learnprogramming/controller/GameController.java
@@ -0,0 +1,48 @@
+package igor.learnprogramming.controller;
+
+import igor.learnprogramming.service.IGameService;
+import igor.learnprogramming.util.AttributeNames;
+import igor.learnprogramming.util.GameMappings;
+import igor.learnprogramming.util.ViewNames;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Slf4j
+@Controller
+public class GameController {
+
+    // == fields ==
+    private final IGameService gameService;
+
+    // == constructors ==
+    @Autowired
+    public GameController(IGameService gameService) {
+        this.gameService = gameService;
+    }
+
+    // == request methods ==
+    @GetMapping(GameMappings.PLAY)
+    public String play(Model model) {
+        model.addAttribute(AttributeNames.MAIN_MESSAGE, gameService.getMainMessage());
+        model.addAttribute(AttributeNames.RESULT_MESSAGE, gameService.getResultMessage());
+        log.info("model = {}", model);
+
+        if (gameService.isGameOver()) {
+            return ViewNames.GAME_OVER;
+        }
+
+        return ViewNames.PLAY;
+    }
+
+    @PostMapping(GameMappings.PLAY)
+    public String processMessage(@RequestParam int guess) {
+        log.info("guess = {}", guess);
+        gameService.checkGuess(guess);
+        return GameMappings.REDIRECT_PLAY;
+    }
+}
